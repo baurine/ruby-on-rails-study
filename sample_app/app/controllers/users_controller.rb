7d1f869a5305c46fd63438c6639b7fb1916eaba2
baurine <2008.hbl@gmail.com>
2015-03-05 11:13:37 +0000

    fix mass assignment error

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -7,4 +7,19 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     #debugger
   end
+  
+  def create
+    @user = User.new(new_params)
+    if @user.save
+      puts 'save ok' # in fact should render another page
+    else
+      puts 'save failed'
+      render 'new'
+    end
+  end
+  
+  private
+    def new_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
