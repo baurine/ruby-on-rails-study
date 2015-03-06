9b54466debed615a175c491b00e550eddd9dbcee
baurine <2008.hbl@gmail.com>
2015-03-06 08:22:55 +0000

    deal with login ok or failed

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # login ok
+    else
+      # login failed
+      # print error message
+      flash[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
   end
   
   def destroy
