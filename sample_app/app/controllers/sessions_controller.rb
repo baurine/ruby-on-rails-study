3bc85ef4abe95adea0d4f81e318399bec8b2b870
baurine <2008.hbl@gmail.com>
2015-03-06 09:19:31 +0000

    add log_in func in SessionsHelper

--- sample_app/app/controllers/sessions_controller.rb
+++ sample_app/app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # login ok
+      log_in user
+      redirect_to user
     else
       # login failed
       # print error message
