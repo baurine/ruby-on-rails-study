72d0a87841c547df37f1e8602d3e125ebd28bccc
baurine <2008.hbl@gmail.com>
2015-03-06 02:03:56 +0000

    show flash message after signup succ

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(new_params)
     if @user.save
       #puts 'save ok' # in fact should render another page
-      puts @user.id
+      #puts @user.id
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user # 是因为当前类是 UsersController，所以 redirect_to 知道是重定向到 /users/id ??
       # 哦，原来这是 redirect_to user_url(@user) 的简写
     else
