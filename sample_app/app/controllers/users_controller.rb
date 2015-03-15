afb95f5e85aaef5313a4b5c756000416b037bb25
baurine <2008.hbl@gmail.com>
2015-03-15 02:30:49 +0000

    admin now can delete user

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only:[:index, :edit, :update]
+  before_action :logged_in_user, only:[:index, :edit, :update, :destroy]
   before_action :correct_user, only:[:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def index
     @users = User.paginate(page: params[:page])
@@ -46,6 +47,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
   private
     def new_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -65,5 +72,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 事前过滤器，确保是管理员用户
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
