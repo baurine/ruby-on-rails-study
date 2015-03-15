9c4af11004d407d09e5ec37485e70ee246c17ecc
baurine <2008.hbl@gmail.com>
2015-03-15 10:52:06 +0000

    start do account activation

--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token
-  before_save { self.email = self.email.downcase }
+  attr_accessor :remember_token, :activation_token
+  
+  before_create :create_activation_digest
+  # before_save { self.email = self.email.downcase }
+  before_save   :downcase_email
+  
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -36,4 +40,14 @@ class User < ActiveRecord::Base
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+    def downcase_email
+      self.email.downcase!
+    end
+    
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 end
