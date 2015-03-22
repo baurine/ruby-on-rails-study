50a707f50e30634e337753649d71b5cc8c5303b5
baurine <2008.hbl@gmail.com>
2015-03-22 10:09:08 +0000

    finish create action

--- sample_app/app/controllers/password_resets_controller.rb
+++ sample_app/app/controllers/password_resets_controller.rb
@@ -2,6 +2,16 @@ class PasswordResetsController < ApplicationController
   def new
   end
 
-  def edit
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "Password reset email has sent"
+      redirect_to root_url
+    else
+      flash[:danger] = "Email address not found"
+      render 'new'
+    end
   end
 end
--- sample_app/app/models/user.rb
+++ sample_app/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   
   before_create :create_activation_digest
   # before_save { self.email = self.email.downcase }
@@ -31,7 +31,6 @@ class User < ActiveRecord::Base
   
   # 验证 token 和 digest j
   def authenticated?(attribute, token)
-    #puts "***************#{remember_digest}$$$$$$$$$$$$$$$$"
     digest = send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
@@ -51,6 +50,16 @@ class User < ActiveRecord::Base
   # 发送激活邮件
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+  
+  def create_reset_digest
+    reset_token = User.new_token
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+  
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
   
   private
